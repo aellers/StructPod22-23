e8c0b6183f12515d3f2d6dda1d65a9e76b60b4a9
aellers <115654510+aellers@users.noreply.github.com>
2022-10-27 08:34:20 +0200

    Add function prototypes

--- 2.c
+++ 2.c
@@ -1,4 +1,24 @@
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>														
+#include<string.h>																										
+#define MAX 64
+
+typedef struct Student * Pozicija;											
+
+struct Student {															
+	char ime[MAX];
+	char prezime[MAX];
+	int godRod;
+	Pozicija next;
+};
+
+void UnosP(char *FName, char *LName, int birthYear, Pozicija P); //unos na pocetak
+void Ispis(Pozicija P);	//ispis liste					
+void UnosK(char *FName, char *LName, int birthYear, Pozicija P); //unos na kraj		
+Pozicija Trazi(char *LName, Pozicija P); //trazi po prezimenu	
+void Brisi(char *LName, Pozicija P); //brisi odredeni element
+
 
 int main()
 {
