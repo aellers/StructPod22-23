b61de38bef56b9d9746242f1efadb62f4a61b75c
aellers <115654510+aellers@users.noreply.github.com>
2022-11-17 18:35:53 +0100

    Add work from 5

--- 5.c
+++ 5.c
@@ -1,3 +1,4 @@
+//in and out from beginning
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,21 +13,25 @@ typedef struct _stack {
 }stack;
 
 
-int Push(position last, int broj);  // prima zadnji el
+int Push(position p, int broj);  // prima zadnji el
 int Pop(position p);  // prima prvi 
 int StanjeStoga(position p);
 
 int main() {
-	stack head = { .el = 0, .next = NULL };
-	position last = &head;
+    stack head = { .el = 0, .next = NULL };
 	char fileName[MAX] = { 0 };
 	FILE* fp = NULL;
 	char line[MAX] = { 0 };
 	char* pline = NULL;
 	int broj = 0;
 	int n = 0;
+	position returnPosition = NULL;
+	//variables for operatons
+	int n1 = 0, n2 = 0;
+    //not a number, not an operator (here)
+	char op[] = ".";
 
-	printf("File name: ");
+    printf("File name: ");
 	scanf(" %s", fileName);
 
 	fp = fopen(fileName, "r");
@@ -35,62 +40,89 @@ int main() {
 		printf("Unable to open file\n");
 		return -1;
 	}
-	fscanf(fp, " %s", line);
+
+    fgets(line, MAX, fp);
+	//test
+	printf("\nlength a: %d\n", (int) strlen(line));
 	pline = line;
 	while (strlen(pline) > 0) {
-		if (sscanf(pline, " %d %n", &broj, &n) == 1) {
-			Push(last, broj);
-		}
-		else {
-			// vidi koji je operator: + - * /
-			// ako je jedan od ova 4 pop zadnje brojeve 
-		}
-		pline += n;
-	}
+		printf("\nlength: %d\n", (int) strlen(pline));
+        sscanf(pline, " %d %n", &broj, &n);
+		if (n == 1) {
+			//test
+			printf("broj: %d\n", broj);
+			Push(&head, broj);
+		} else {
+            sscanf(pline, "%c %n", op, &n);
+            printf("\nop: %s\n", op);
+            if (strcmp(op, "+") == 0 || strcmp(op, "-") == 0 ||
+            strcmp(op, "*") == 0 || strcmp(op, "/") == 0 ) {
+                n2 = Pop(&head);
+                n1 = Pop(&head);
+                if (strcmp(op, "+") == 0 ) {
+                    Push(&head, n1+n2);
+                } else if (strcmp(op, "-") == 0) {
+                    Push(&head, n1 - n2);
+                } else if (strcmp(op, "*") == 0) {
+                    Push(&head, n1 * n2);
+                } else if (strcmp(op, "/") == 0) {
+                    Push(&head, n1 - n2);
+                } 
+                
+            }
+		
+	    }
+        pline += n;
+    }
+    StanjeStoga(&head);
+    fclose(fp);
+    return 0;
+}
 
 
-		fclose(fp);
-		//free all memory allocaated for stack
-
-		return 0;
-	}
-
-
-//p and last are same
-int Push(position last, int broj)
-{
-	position el = NULL, p = last;
+//&head
+int Push(position p, int broj) {
+    position el = NULL;
 	el = malloc(sizeof(stack));
 	if (el == NULL) {
 		printf("Unable to allocate memory\n");
 		return -1;
 	}
-	p->next = el->next;
-	el->next = NULL;
-	el->el = broj;
-
-	last = el;
-
-	return 0;
+    el->next = p->next;
+    p->next = el;
+    el->el = broj;
+    return 0;
 }
 
-int Pop(position p)
-{
+//&head
+int Pop(position p) {
+    position temp = NULL;
+    int broj = 0;
+    if (p->next == NULL) {
+        printf("Prazni stog\n");
+        return -1;
+    }
+    temp = p->next;
+    p->next = p->next->next;
+    broj = temp->el;
+    free(temp);
 
-	//postavi pokazivace,izbrisat(free)
-
-	return 0;
+    return broj;
 }
+
 
 //isprinta elemente stoga
 int StanjeStoga(position p) {
 	if (p->next == NULL) {
-		printf("Prazni stog");
+		printf("\nPrazni stog\n");
 		return -1;
 	}
+	printf("\n\n");
+	
 	while (p->next != NULL) {
-		printf("%d\n", p->next->el);
+		printf("%d\t", p->next->el);
 		p = p->next;
 	}
+	puts("");
 	return 0;
 }
