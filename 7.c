37d9eb86acb154d7a4fec1abf23b4eade40266bf
aellers <115654510+aellers@users.noreply.github.com>
2022-12-09 17:08:56 +0100

    Add stack structure (without functions)

--- 7.c
+++ 7.c
@@ -4,18 +4,34 @@
 #define MAX_FILENAME_LENGTH 12
 
 typedef struct node node;
-typedef struct node* position;
+typedef struct node* treePos;
 
 typedef struct node {
     char name[MAX_FILENAME_LENGTH];
-    position nextSibling;
-    position firstChild;
+    treePos nextSibling;
+    treePos firstChild;
 } node;
 
-int createSubdirectory(position p, char* dirName);
-int createDirectory(position p, char* dirName);
+//stack for traversal and output; stores pointers to nodes of tree
+typedef struct stack stack;
+typedef struct stack* stackPos;
+
+typedef struct stack {
+    node el;
+    //element put on stack before 
+    stackPos pointsTo;
+} stack;
+
+
+int createSubdirectory(treePos p, char* dirName);
+int createDirectory(treePos p, char* dirName);
+
+treePos push(stackPos p, treePos el);
+treePos pop();
 
 int main() {
+    stack head = {.el = NULL, .pointsTo = NULL};
+    treePos rightmost = NULL;
     node root = {.nextSibling = NULL, .firstChild = NULL };
     strcpy(root.name, "root");
     
@@ -23,8 +39,8 @@ int main() {
 }
 
 //create child
-int createSubdirectory(position p, char* dirName) {
-    position q = NULL;
+int createSubdirectory(treePos p, char* dirName) {
+    treePos q = NULL;
 
     q = (node*) malloc(sizeof(node));
     if (q == NULL) {
@@ -48,8 +64,8 @@ int createSubdirectory(position p, char* dirName) {
 }
 
 //create sibling
-int createDirectory(position p, char* dirName) {
-    position q = NULL;
+int createDirectory(treePos p, char* dirName) {
+    treePos q = NULL;
 
     q = (node*) malloc(sizeof(node));
     if (q == NULL) {
